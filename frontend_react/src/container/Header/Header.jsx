517c87ffc3d68f6d2adb317817d1bf2ecd081287
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-02-26 14:15:02 +0600

    image in header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
+import { images } from '../../constants'
 import './Header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 1.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Name</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className='tag-cmp app__flex'>
+          <p className='p-text'>Student</p>
+          <p className='p-text'>Developer</p>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile_bg' />
+
+      </motion.div>
+
+      <motion.div></motion.div>
+    </div>
   )
 }
 
