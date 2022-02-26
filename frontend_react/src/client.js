6314e5803223c2008f49121f65ed9356a6771c2a
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-02-26 23:07:09 +0600

    sanity setup added

---
+++ frontend_react/src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const client = sanityClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2022-02-01',
+  useCdn: true,
+  token: process.env.SANITY_TOKEN
+})
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source)
