93c498fc2d5d54d8979f3d082d066e279995b0c4
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-03-02 01:10:47 +0600

    testimonial work ongoing

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 
+import { AppWrap, MotionWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
 import './Testimonial.scss'
 
 const Testimonial = () => {
+  const [ brands, setBrands ] = useState([])
+  const [ testimonials, setTestimonials ] = useState([])
+  const [ currentIndex, setCurrentIndex ] = useState(true)
+
+  useEffect(() => {
+    const testimonialsQuery = '*[_type == "testimonials"]'
+    const brandsQuery = '*[_type == "brands"]'
+    
+    client.fetch(testimonialsQuery).then((data) => {
+      setTestimonials(data)
+    })
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data)
+    })
+    
+  }, [])
+  
   return (
     <div>Testimonial</div>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  "app__primarybg"
+)
