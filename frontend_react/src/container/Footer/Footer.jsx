7065d9108401737e833409e6e2cd2b9eb17bbbfc
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-03-02 21:56:11 +0600

    footer work started

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { images } from '../../constants'
+import { AppWrap, MotionWrap } from '../../wrapper'
+import { client } from '../../client'
 import './Footer.scss' 
 
 const Footer = () => {
@@ -8,4 +11,8 @@ const Footer = () => {
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+)
